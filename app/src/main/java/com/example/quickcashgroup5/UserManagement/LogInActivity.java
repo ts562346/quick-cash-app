426f9db0efe612b0de5351c47666cd3294020696
mirazul <mr779801@dal.ca>
2021-10-16 23:57:58 -0300

    Frontend and Backend for regiestration

--- app/src/main/java/com/example/quickcashgroup5/UserManagement/LogInActivity.java
+++ app/src/main/java/com/example/quickcashgroup5/UserManagement/LogInActivity.java
@@ -1,14 +1,23 @@
 package com.example.quickcashgroup5.UserManagement;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quickcashgroup5.R;
 
 public class LogInActivity extends AppCompatActivity {
-
-
-
+    Button  registerButton;
+    EditText email,
+            password;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        password = (EditText) findViewById(R.id.editTextTextPassword);
+    }
 }
