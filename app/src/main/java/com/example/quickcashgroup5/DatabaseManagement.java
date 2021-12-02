f3f99bba7308c75688fe09a027c13043ab5273d9
charolia <sz500426@dal.ca>
2021-12-02 06:53:21 -0400

    recreated isprevioususer function in DatabaseManagement

---
+++ app/src/main/java/com/example/quickcashgroup5/DatabaseManagement.java
@@ -0,0 +1,48 @@
+package com.example.quickcashgroup5;
+
+import androidx.annotation.NonNull;
+
+import com.example.quickcashgroup5.UserManagement.Feedback;
+import com.example.quickcashgroup5.UserManagement.User;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class DatabaseManagement {
+    FirebaseDatabase database;
+
+    protected DatabaseManagement() {
+        //initialize the database and the two references related to banner ID and email address.
+        database = FirebaseDatabase.getInstance("https://quickcashgroupproject-default-rtdb.firebaseio.com/");
+    }
+
+    public boolean isPreviousUser(String email) {
+        DatabaseReference users = database.getReference(User.class.getSimpleName());
+
+        AtomicBoolean userExists = new AtomicBoolean(false);
+        users.child("User").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot adSnapshot : dataSnapshot.getChildren()) {
+                    User u = adSnapshot.getValue(User.class);
+                    if (u.getEmail().equals(email)) {
+                        System.out.println("Old User");
+                        userExists.set(true);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("New User");
+            }
+        });
+        return userExists.get();
+    }
+}
