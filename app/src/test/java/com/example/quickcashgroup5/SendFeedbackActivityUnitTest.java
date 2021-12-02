e6db866b11da9a64e7a5ba9ed8d9c9c9d5c5553a
Aks Chunara <ak278582@dal.ca>
2021-12-02 06:05:08 -0400

    Fixed test cases

--- app/src/test/java/com/example/quickcashgroup5/SendFeedbackActivityUnitTest.java
+++ app/src/test/java/com/example/quickcashgroup5/SendFeedbackActivityUnitTest.java
@@ -1,40 +1,30 @@
 //package com.example.quickcashgroup5;
 //
-//import static org.hamcrest.core.IsInstanceOf.instanceOf;
-//import static org.hamcrest.core.StringContains.containsString;
 //import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
 //
-//import androidx.annotation.NonNull;
-////import androidx.test.espresso.action.ViewActions;
-////import androidx.test.ext.junit.rules.ActivityScenarioRule;
-//
-//import com.example.quickcashgroup5.UserManagement.CreateJob;
 //import com.example.quickcashgroup5.UserManagement.JobPosting;
-//import com.example.quickcashgroup5.UserManagement.LogInActivity;
 //import com.example.quickcashgroup5.UserManagement.SessionManagement;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
 //
 //import org.junit.BeforeClass;
 //import org.junit.Test;
 //import org.mockito.Mockito;
 //
-//public class CreateJobUnitTest {
-//    private String jobTitle = "1234567890";
-//    private String location = "Halifax, Nova Scotia";
-//    private static String creatorEmail = "espresso@bot.test";
-//    private String payment = "15";
-//    private String duration = "12";
-//    private String category = "Computer Repair";
+//public class SendFeedbackActivityUnitTest {
+//    private static SessionManagement sessionManagement;
+//    private static String name = "JohnDoe";
+//    private static String email = "john.doe@gmail.com";
+//    private static String userType = "Employee";
+//    private static String userFeedback = "Great app!";
+//    private static int rating = 5;
 //
 //    @BeforeClass
 //    public static void setup() {
-//        //Mock the IPromoOffer interface
+//        sessionManagement = Mockito.mock(SessionManagement.class);
+//        Mockito.when(sessionManagement.getName()).thenReturn(name);
+//        Mockito.when(sessionManagement.getEmail()).thenReturn(email);
+//        Mockito.when(sessionManagement.getRole()).thenReturn(userType);
+//        Mockito.when(sessionManagement.isLoggedIn()).thenReturn(true);
+//
 //        firstOffer = Mockito.mock(IPromoOffer.class);
 //        Mockito.when(firstOffer.getOffer()).thenReturn("This is a cool offer!");
 //        Mockito.when(firstOffer.getExpiredDate()).thenReturn("2021-12-31");
@@ -43,13 +33,12 @@
 //    }
 //
 //    @Test
-//    public void checkIfJobIsInserted() {
+//    public void checkIfFeedbackIsInserted() {
 //        editTextTitle.setText(jobTitle);
 //        editTextLocation.setText(location);
 //        editTextPayment.setText(payment);
 //        editTextDuration.setText(duration);
 //        spinnerCategory.setSelection(2);
-//        sessionManagement.setEmail(creatorEmail);
 //
 //        insertJob();
 //
@@ -62,10 +51,16 @@
 //        assertEquals(job.getPayment(), payment);
 //        assertEquals(job.getSelectedApplicantEmail(), "");
 //
+//        Mockito.verify(sessionManagement, Mockito.atLeastOnce()).getName();
+//        Mockito.verify(sessionManagement, Mockito.atLeastOnce()).getEmail();
+//        Mockito.verify(sessionManagement, Mockito.atLeastOnce()).getRole();
+//        Mockito.verify(sessionManagement, Mockito.atLeastOnce()).isLoggedIn();
+//
 //        Mockito.verify(, Mockito.atLeastOnce()).getCategory();
 //        Mockito.verify(, Mockito.atLeastOnce()).getCreatorEmail();
 //        Mockito.verify(, Mockito.atLeastOnce()).getDuration();
 //        Mockito.verify(, Mockito.atLeastOnce()).getLocation();
 //        Mockito.verify(, Mockito.atLeastOnce()).getPayment();
 //        Mockito.verify(, Mockito.atLeastOnce()).getSelectedApplicantEmail();
+//    }
 //}
